2edfe9b65921f4ceb23bf66b3f2845b9cca71b73
Keaton Sentak <k.sentak@outlook.com>
2021-01-02 18:27:46 -0500

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="amber" dark>
+      <div class="d-flex align-center">
+        <h2>Vue Notes</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <!-- CAN ADD V-BTN HERE -->
+    </v-app-bar>
+
+    <v-main>
+      <!-- Where notes will be added -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({})
+};
+</script>
